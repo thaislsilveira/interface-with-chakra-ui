6dd899cfe6ef55f7dac1996d0a73e97e1d4c340d
Thaís Silveira <thais_lsilveira@hotmail.com>
2021-04-08 15:47:27 -0300

    feat: add Banner

---
+++ src/components/Dashboard/Banner.tsx
@@ -0,0 +1,39 @@
+import { Flex, Box, Image, Text } from '@chakra-ui/react'
+
+export function Banner() {
+  return (
+    <Flex
+      w="100%"
+      h="335px"
+      bgImage="url('/images/background.png')"
+      bgSize="cover"
+      pos="relative"
+      align="center"
+      pl={{
+        base: '16px',
+        md: '140px'
+      }}
+    >
+      <Box>
+        <Text color="gray.100" fontSize="36" fontWeight="500">
+          5 Continentes,
+          <br />
+          infinitas possibilidades.
+        </Text>
+        <Text color="gray.200" fontSize="20" mt="20px">
+          Chegou a hora de tirar do papel a viagem que você
+          <br />
+          sempre sonhou.
+        </Text>
+      </Box>
+
+      <Image
+        pos="absolute"
+        bottom="-10"
+        right="140"
+        src="/images/airplane.svg"
+        alt="Avião"
+      />
+    </Flex>
+  )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,16 @@
-import { Box } from '@chakra-ui/react'
+import { Box, Flex } from '@chakra-ui/react'
 
 import { Header } from '../components/Header'
 
+import { Dashboard } from '../components/Dashboard'
+
 export default function Home() {
-  return <Header />
+  return (
+    <Box>
+      <Header />
+      <Flex direction="column" pb="40px">
+        <Dashboard />
+      </Flex>
+    </Box>
+  )
 }
